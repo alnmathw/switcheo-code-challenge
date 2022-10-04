3764cfc0d18c95436e60c1428687432a6cc96a02
“alnmathw” <alan.matthew.anggara@gmail.com>
2022-10-04 21:02:19 +0800

    problem 2: finished

---
+++ src/problem4/retrieve-holders.ts
@@ -0,0 +1,295 @@
+import { ethers } from "ethers";
+
+/* Initialize Variables */
+const SWTH_TOKEN_CONTRACT = '0xc0ecb8499d8da2771abcbf4091db7f65158f1468';
+const ADDRESSES_TO_LOOK_UP = [
+  '0xb5d4f343412dc8efb6ff599d790074d0f1e8d430',
+  '0x0020c5222a24e4a96b720c06b803fb8d34adc0af',
+  '0xd1d8b2aae2ebb2acf013b803bc3c24ca1303a392'
+];
+
+const SWTH_CONTRACT_ABI = [
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'owner',
+				type: 'address'
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'spender',
+				type: 'address'
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'value',
+				type: 'uint256'
+			}
+		],
+		name: 'Approval',
+		type: 'event'
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'from',
+				type: 'address'
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'to',
+				type: 'address'
+			},
+			{
+				indexed: false,
+				internalType: 'uint256',
+				name: 'value',
+				type: 'uint256'
+			}
+		],
+		name: 'Transfer',
+		type: 'event'
+	},
+	{
+		constant: true,
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_owner',
+				type: 'address'
+			},
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address'
+			}
+		],
+		name: 'allowance',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'spender',
+				type: 'address'
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256'
+			}
+		],
+		name: 'approve',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool'
+			}
+		],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'account',
+				type: 'address'
+			}
+		],
+		name: 'balanceOf',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'decimals',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'getOwner',
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'name',
+		outputs: [
+			{
+				internalType: 'string',
+				name: '',
+				type: 'string'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'symbol',
+		outputs: [
+			{
+				internalType: 'string',
+				name: '',
+				type: 'string'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'totalSupply',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'recipient',
+				type: 'address'
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256'
+			}
+		],
+		name: 'transfer',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool'
+			}
+		],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'sender',
+				type: 'address'
+			},
+			{
+				internalType: 'address',
+				name: 'recipient',
+				type: 'address'
+			},
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256'
+			}
+		],
+		name: 'transferFrom',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool'
+			}
+		],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	}
+];
+
+const PROVIDER_ADDRESS = 'https://bsc-dataseed.binance.org/'
+
+/* Initialize Provider to Access Data */
+const provider = new ethers.providers.JsonRpcProvider(
+  PROVIDER_ADDRESS
+);
+
+const token_contract = new ethers.Contract(SWTH_TOKEN_CONTRACT, SWTH_CONTRACT_ABI, provider);
+
+const getTokenBalanceFromAddress = async (addr : string) => {
+  try {
+    const balance = await token_contract.balanceOf(addr);
+    return balance;
+  }
+  catch (error) {
+    throw error;
+  }
+};
+
+(
+  async () => {
+    for (const addr of ADDRESSES_TO_LOOK_UP) {
+      const balance = await getTokenBalanceFromAddress(addr);
+      console.log(`${addr} ${parseFloat(ethers.utils.formatUnits(balance, 8)).toLocaleString('en-SG', { minimumFractionDigits: 8 })}`)
+    }
+  }
+)();
